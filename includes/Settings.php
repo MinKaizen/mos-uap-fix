fb8078e08040f8a73dd9dab9fd9635a9eaf59243
Martin Cao <martin.g.cao@gmail.com>
2021-10-19 16:24:40 +1100

    Add Settings class and admin settings page

---
+++ includes/Settings.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+namespace MOS_UAP_Fix;
+
+use function \get_option;
+
+class Settings {
+	const OPTION_NAME = 'mos_uap_fix_settings';
+	const PARAM_NAME = 'param_name';
+	const PARAM_IDENTIFIER = 'param_identifier';
+	const COOKIE_NAME = 'cookie_name';
+	const COOKIE_EXPIRATION_DAYS = 'cookie_expiration_days';
+	const AUTO_SET_USER_AS_AFFILIATE = 'auto_set_user_as_affiliate';
+	const DEFAULTS = [
+		'param_name' => 'id',
+		'param_identifier' => 'username',
+		'cookie_name' => 'mos_sponsor_wpid',
+		'cookie_expiration_days' => 360,
+		'auto_set_user_as_affiliate' => false,
+	];
+
+	private static ?self $instance;
+	private array $options;
+
+	public function __construct() {
+		$options = get_option( self::OPTION_NAME );
+		$this->options = is_array($options) ? $options : [];
+	}
+
+	public static function instance(): self {
+		$instance = self::$instance ?? new self();
+		self::$instance = $instance;
+		return $instance;
+	}
+
+	public function init(): void {
+		include('settings-page.php');
+	}
+
+	public function get_param_name(): string {
+		$param_name = $this->options[self::PARAM_NAME] ?? self::DEFAULTS[self::PARAM_NAME];
+		return (string) $param_name;
+	}
+
+	public function get_param_identifier(): string {
+		$param_identifier = $this->options[self::PARAM_IDENTIFIER] ?? self::DEFAULTS[self::PARAM_IDENTIFIER];
+		return (string) $param_identifier;
+	}
+
+	public function get_cookie_name(): string {
+		$cookie_name = $this->options[self::COOKIE_NAME] ?? self::DEFAULTS[self::COOKIE_NAME];
+		return (string) $cookie_name;
+	}
+
+	public function get_cookie_expiration_days(): int {
+		$cookie_expiration_days = $this->options[self::COOKIE_EXPIRATION_DAYS] ?? self::DEFAULTS[self::COOKIE_EXPIRATION_DAYS];
+		return (int) $cookie_expiration_days;
+	}
+
+	public function get_auto_set_user_as_affiliate(): bool {
+		$auto_set_user_as_affiliate = $this->options[self::AUTO_SET_USER_AS_AFFILIATE] ?? self::DEFAULTS[self::AUTO_SET_USER_AS_AFFILIATE];
+		return (bool) $auto_set_user_as_affiliate;
+	}
+}
---
+++ includes/settings-page.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace MOS_UAP_Fix;
+
+require_once('Settings.php');
+
+add_action( 'admin_menu', NS . 'mos_uap_fix_add_admin_menu' );
+add_action( 'admin_init', NS . 'mos_uap_fix_settings_init' );
+
+function mos_uap_fix_add_admin_menu() {
+	add_options_page( 'UAP Fix', 'UAP Fix', 'manage_options', 'mos_uap_fix', NS . 'mos_uap_fix_options_page' );
+}
+
+function mos_uap_fix_settings_init() {
+	register_setting( 'pluginPage', 'mos_uap_fix_settings' );
+
+	add_settings_section(
+		'mos_uap_fix_pluginPage_section',
+		__( 'UAP Fix Settings', 'mos-uap-fix' ),
+		NS . 'mos_uap_fix_settings_section_callback',
+		'pluginPage'
+	);
+
+	add_settings_field(
+		'param_name',
+		__( 'Affiliate Link Parameter Name', 'mos-uap-fix' ),
+		NS . 'param_name_setting_render',
+		'pluginPage',
+		'mos_uap_fix_pluginPage_section'
+	);
+
+	add_settings_field(
+		'param_identifier',
+		__( 'Affiliate Link Parameter Identifier', 'mos-uap-fix' ),
+		NS . 'param_identifier_setting_render',
+		'pluginPage',
+		'mos_uap_fix_pluginPage_section'
+	);
+
+	add_settings_field(
+		'cookie_expiration_days',
+		__( 'Cookie Expiration (days)', 'mos-uap-fix' ),
+		NS . 'cookie_expiration_days_setting_render',
+		'pluginPage',
+		'mos_uap_fix_pluginPage_section'
+	);
+
+	add_settings_field(
+		'auto_set_user_as_affiliate',
+		__( 'Automatically make new user affiliates', 'mos-uap-fix' ),
+		NS . 'auto_set_user_as_affiliate_setting_render',
+		'pluginPage',
+		'mos_uap_fix_pluginPage_section'
+	);
+}
+
+function param_name_setting_render() {
+	$settings = Settings::instance();
+	$param_name = $settings->get_param_name();
+	?>
+	<input type='text' name='<?php echo Settings::OPTION_NAME . "[" . Settings::PARAM_NAME . "]"; ?>' value='<?php echo $param_name; ?>' required>
+	<span class="note">e.g. If set to <code>id</code>, affiliate links will become <code><?php echo home_url();?>/?id=blah</code></span>
+	<?php
+}
+
+function param_identifier_setting_render() {
+	$settings = Settings::instance();
+	$param_identifier = $settings->get_param_identifier();
+	?>
+	<select name='<?php echo Settings::OPTION_NAME . "[" . Settings::PARAM_IDENTIFIER . "]"; ?>'>
+		<option value="username" <?php echo ($param_identifier == 'username' ? 'selected' : ''); ?>>Username</option>
+		<option value="wpid" <?php echo ($param_identifier == 'wpid' ? 'selected' : ''); ?>>WPID</option>
+		<option value="email" <?php echo ($param_identifier == 'email' ? 'selected' : ''); ?>>Email</option>
+		<option value="affid" <?php echo ($param_identifier == 'affid' ? 'selected' : ''); ?>>UAP Aff ID</option>
+	</select>
+	<span class="note">What should be used in the affiliate link to identify an affiliate?</span>
+	<?php
+}
+
+function cookie_expiration_days_setting_render() {
+	$settings = Settings::instance();
+	$cookie_expiration_days = $settings->get_cookie_expiration_days();
+	?>
+	<input type='number' required step="1" min="0" name='<?php echo Settings::OPTION_NAME . "[" . Settings::COOKIE_EXPIRATION_DAYS . "]"; ?>' value='<?php echo $cookie_expiration_days; ?>'>
+	<span class="note">(Changes only apply to new cookies)</span>
+	<?php
+}
+
+function auto_set_user_as_affiliate_setting_render(  ) {
+	$settings = Settings::instance();
+	$auto_set_user_as_affiliate = $settings->get_auto_set_user_as_affiliate();
+	?>
+	<input type='checkbox' name='<?php echo Settings::OPTION_NAME . "[" . Settings::AUTO_SET_USER_AS_AFFILIATE . "]"; ?>' <?php checked( $auto_set_user_as_affiliate, 1 ); ?> value='1'>
+	<span class="note">Only enable this if UAP setting <code>Automatically Affiliate</code> is off or not working</span>
+	<?php
+}
+
+
+function mos_uap_fix_settings_section_callback() {}
+
+function mos_uap_fix_options_page() {
+	?>
+	<style>
+		.note {
+			font-style: italic;
+		}
+	</style>
+	<form action='options.php' method='post'>
+		<?php
+		settings_fields( 'pluginPage' );
+		do_settings_sections( 'pluginPage' );
+		submit_button();
+		?>
+	</form>
+	<?php
+}
