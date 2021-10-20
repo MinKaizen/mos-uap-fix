b76e8419f43a58731e14ff0288c6d83e84f6c775
Martin Cao <martin.g.cao@gmail.com>
2021-10-20 14:38:52 +1100

    Fix comment typo

--- mos-uap-fix.php
+++ mos-uap-fix.php
@@ -35,7 +35,7 @@ define( NS . 'PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( NS . 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( NS . 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
 
-// Github Update constans
+// Github Update constants
 define( NS . 'GITHUB_USER', 'MinKaizen' );
 define( NS . 'GITHUB_REPO', 'mos-uap-fix' );
 
