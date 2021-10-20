c451978df661f793a6a532d2c74541a3c3eee2b7
Martin Cao <martin.g.cao@gmail.com>
2021-10-20 15:12:17 +1100

    Update version number

--- mos-uap-fix.php
+++ mos-uap-fix.php
@@ -6,14 +6,14 @@ namespace MOS_UAP_Fix;
  * Main Plugin File
  *
  * @link              https://github.com/MinKaizen/mos-uap-fix
- * @since             1.1.0
+ * @since             1.2.0
  * @package           MOS_UAP_Fix
  *
  * @wordpress-plugin
  * Plugin Name:       MOS UAP Fix
  * Plugin URI:        https://github.com/MinKaizen/mos-uap-fix
  * Description:       Hacky fix for UAP plugin (Indeed Ultimate Affiliate Pro)
- * Version:           1.1.0
+ * Version:           1.2.0
  * Author:            MinKaizen
  * Author URI:        https://github.com/MinKaizen/
  * License:           GPL-2.0+
@@ -33,7 +33,7 @@ date_default_timezone_set('Australia/Sydney');
 // Plugin constants
 define( __NAMESPACE__ . '\NS', __NAMESPACE__ . '\\' );
 define( NS . 'PLUGIN_NAME', 'mos-uap-fix' );
-define( NS . 'PLUGIN_VERSION', '1.0.0' );
+define( NS . 'PLUGIN_VERSION', '1.2.0' );
 define( NS . 'PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( NS . 'PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( NS . 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
